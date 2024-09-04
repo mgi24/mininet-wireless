8db6f0188779472f0052e87c68e46d480a0bf94a
mgi24 <ahmadmishbah@student.uns.ac.id>
2024-09-04 22:08:39 +0700

    ready to test

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from mn_wifi.cli import CLI
 from mn_wifi.net import Mininet_wifi
 from mn_wifi.wmediumdConnector import interference
 from mininet.node import Controller,  Host,  OVSKernelSwitch
-
+from mininet.link import Intf
 
 def topology():
     "Create a network."
@@ -16,33 +16,54 @@ def topology():
 
     info("*** Creating nodes\n")
     s1 = net.addSwitch('s1', cls=OVSKernelSwitch, failMode='standalone')
-    h1 = net.addHost('h1', ip = '192.168.1.125/24')
-    h2 = net.addHost('h2', ip = '192.168.1.130/24')
+    h1 = net.addHost('h1', ip = '0.0.0.0')
+    h2 = net.addHost('h2', ip = '0.0.0.0')
     
 
-    sta1 = net.addStation('sta1', ip = '192.168.1.120/24',  position = '100.0,60.0,0.0')
-    sta2 = net.addStation('sta2', ip = '192.168.1.122/24', position = '0.0,60.0,0.0')
-
-
+    sta1 = net.addStation('sta1', ip = '0.0.0.0',  position = '60.0,60.0,0.0')
+    sta2 = net.addStation('sta2', ip = '0.0.0.0', position = '0.0,60.0,0.0')
     ap1 = net.addAccessPoint('ap1', ssid="simplewifi", mode="g", channel="1", position = '20.0,60.0,0.0')
+    net.setPropagationModel(model="logDistance", exp=4.5)
+    
     c0 = net.addController('c0')
+    
     info("*** Configuring nodes\n")
     net.configureNodes()
 
     info("*** Associating Stations\n")
-    #net.addLink(sta1, ap1)
-    #net.addLink(sta2, ap1)
+    net.addLink(sta1, ap1)
+    net.addLink(sta2, ap1)
     net.addLink(s1, ap1)
     net.addLink(s1, h1)
     net.addLink(s1, h2)
+
+    info('*** Adding physical interface ens33 >===< switch\n')
+    intf = Intf('ens33', node=s1)
 
     info("*** Plotting Graph\n")
     net.plotGraph(max_x=120, max_y=120)
 
     info("*** Starting network\n")
     net.build()
+    c0.start()
     ap1.start([c0])
     s1.start([c0])
+
+    ap1.cmd('echo 1 > /proc/sys/net/ipv4/ip_forward')
+    h1.cmd('dhclient h1-eth0')
+    h2.cmd('dhclient h2-eth0')
+    #sta1.cmd('dhclient sta1-wlan0')
+    #sta2.cmd('dhclient sta2-wlan0')
+
+    info('*** Checking IP address host\n')
+    print(f"IP h1:{h1.cmd('hostname -I')}")
+    print(f"IP h2:{h2.cmd('hostname -I')}")
+    print(f"IP sta1:{sta1.cmd('hostname -I')}")
+    print(f"IP sta2:{sta2.cmd('hostname -I')}")
+    info('*** Changing DNS server\n')
+    #h1.cmd("echo 'nameserver 192.168.1.1' > /etc/resolv.conf")
+    
+    
 
     info("*** Running CLI\n")
     CLI(net)
--- test2.py
+++ test2.py
@@ -8,7 +8,7 @@ from mn_wifi.cli import CLI
 from mn_wifi.net import Mininet_wifi
 from mn_wifi.wmediumdConnector import interference
 from mininet.node import Controller,  Host,  OVSKernelSwitch
-
+from mininet.link import Intf
 
 def topology():
     "Create a network."
@@ -16,14 +16,17 @@ def topology():
 
     info("*** Creating nodes\n")
     s1 = net.addSwitch('s1', cls=OVSKernelSwitch, failMode='standalone')
-    h1 = net.addHost('h1', ip = '192.168.1.125/24')
-    h2 = net.addHost('h2', ip = '192.168.1.130/24')
+    h1 = net.addHost('h1', ip = '0.0.0.0')
+    h2 = net.addHost('h2', ip = '0.0.0.0')
     
 
-    sta1 = net.addStation('sta1', ip = '192.168.1.120/24',  position = '100.0,60.0,0.0')
-    sta2 = net.addStation('sta2', ip = '192.168.1.122/24', position = '0.0,60.0,0.0')
+    sta1 = net.addStation('sta1', ip = '0.0.0.0',  position = '60.0,60.0,0.0')
+    sta2 = net.addStation('sta2', ip = '0.0.0.0', position = '0.0,60.0,0.0')
     ap1 = net.addAccessPoint('ap1', ssid="simplewifi", mode="g", channel="1", position = '20.0,60.0,0.0')
+    net.setPropagationModel(model="logDistance", exp=4.5)
+    
     c0 = net.addController('c0')
+    
     info("*** Configuring nodes\n")
     net.configureNodes()
 
@@ -33,6 +36,9 @@ def topology():
     net.addLink(s1, ap1)
     net.addLink(s1, h1)
     net.addLink(s1, h2)
+
+    info('*** Adding physical interface ens33 >===< switch\n')
+    intf = Intf('ens33', node=s1)
 
     info("*** Plotting Graph\n")
     net.plotGraph(max_x=120, max_y=120)
@@ -44,6 +50,20 @@ def topology():
     s1.start([c0])
 
     ap1.cmd('echo 1 > /proc/sys/net/ipv4/ip_forward')
+    h1.cmd('dhclient h1-eth0')
+    h2.cmd('dhclient h2-eth0')
+    #sta1.cmd('dhclient sta1-wlan0')
+    #sta2.cmd('dhclient sta2-wlan0')
+
+    info('*** Checking IP address host\n')
+    print(f"IP h1:{h1.cmd('hostname -I')}")
+    print(f"IP h2:{h2.cmd('hostname -I')}")
+    print(f"IP sta1:{sta1.cmd('hostname -I')}")
+    print(f"IP sta2:{sta2.cmd('hostname -I')}")
+    info('*** Changing DNS server\n')
+    #h1.cmd("echo 'nameserver 192.168.1.1' > /etc/resolv.conf")
+    
+    
 
     info("*** Running CLI\n")
     CLI(net)
